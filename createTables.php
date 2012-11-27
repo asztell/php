be947b2ff833444be53353138ee9936f15c9063b
asztell <asztell@yahoo.com>
2012-11-27 15:49:12 -0800

    sql

--- createTables.php
+++ createTables.php
@@ -1,10 +1,10 @@
 <?php
 	include('db_connection_info.inc');
 	$conn = mysqli_connect('localhost', $cs85Username, $cs85Password, 'albums');
-	$delete = mysqli_query($conn, 'delete table asztalos_arpad_attil_artist');
-	$result = mysqli_query($conn, 'create table asztalos_arpad_attil_artist (firstName varchar(15), lastName varchar(15));');
-	if (!$result) 
+	$result = mysqli_query($conn, 'create table if not exists asztalos_arpad_attil_artist (firstName varchar(15), lastName varchar(15));');
+	if (!$result) {
 		echo mysqli_error($conn);
+	}
 /*	} else {
 		echo "<pre>";
 		while ($row = $result->fetch_assoc()) {
@@ -15,7 +15,7 @@
 		}
 		echo "</pre>";
 	}*/
-	else 
+	else {
 		echo "database created";
-
+	}
 ?>
