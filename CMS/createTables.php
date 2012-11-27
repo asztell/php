0255cade8306fc1aee048e7019c8cc12278f05aa
asztell <asztell@yahoo.com>
2012-11-27 15:53:40 -0800

    sql

--- CMS/createTables.php
+++ CMS/createTables.php
@@ -1,21 +1,10 @@
 <?php
-	include('db_connection_info.inc');
-	$conn = mysqli_connect('localhost', $cs85Username, $cs85Password, 'albums');
-	$delete = mysqli_query($conn, 'delete table asztalos_arpad_attil_artist');
-	$result = mysqli_query($conn, 'create table asztalos_arpad_attil_artist (firstName varchar(15), lastName varchar(15));');
-	if (!$result) 
-		echo mysqli_error($conn);
-/*	} else {
-		echo "<pre>";
-		while ($row = $result->fetch_assoc()) {
-			foreach ($row as $k => $v) {
-				echo $v."\t";
-			}
-			echo "\n";
-		}
-		echo "</pre>";
-	}*/
-	else 
-		echo "database created";
-
+	include( 'db_connection_info.inc' );
+	$conn = mysqli_connect( 'localhost', $cs85Username, $cs85Password, 'albums' );
+	$result = mysqli_query( $conn, 'create table if not exists asztalos_arpad_attil_artist ( firstName varchar( 15 ), lastName varchar( 15 ) );' );
+	if ( !$result ) {
+		echo mysqli_error( $conn );
+	} else {
+		echo "table created";
+	}
 ?>
